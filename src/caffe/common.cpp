f7498789d554520d7711f32b8671f1e12343be3e
Shaoqing Ren <sqren@mail.ustc.edu.cn>
2014-10-07 21:19:12 +0800

    support spp mex

--- src/caffe/common.cpp
+++ src/caffe/common.cpp
@@ -93,7 +93,7 @@ void* Caffe::RNG::generator() {
 
 Caffe::Caffe()
     : cublas_handle_(NULL), curand_generator_(NULL), random_generator_(),
-    mode_(Caffe::CPU), phase_(Caffe::TRAIN), gpu_mode_(GPU_Mode::GPU_AVAILABLE) {
+    mode_(Caffe::CPU), phase_(Caffe::TRAIN), gpu_mode_(Caffe::GPU_AVAILABLE) {
   // Try to create a cublas handler, and report an error if failed (but we will
   // keep the program running as one might just want to run CPU code).
   if (cublasCreate(&cublas_handle_) != CUBLAS_STATUS_SUCCESS) {
